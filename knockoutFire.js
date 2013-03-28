18e68f2a0ec50258b66e7c7bcf4d573caeaa80e4
Hiroshi Saito <hiroshi3110@gmail.com>
2013-03-29 00:00:16 +0900

    Implemented KnockoutFire.observableArray() instead of KnockoutFireCollectionViewModel().

--- knockoutFire.js
+++ knockoutFire.js
@@ -5,24 +5,28 @@
 
   Basic Usage Example:
     var firebaseRef = new Firebase("https://yourdb.firebaseio.com/items");
-    var itemsViewModel = new KnockoutFireCollectionViewModel(firebaseRef);
+    var viewModel = {
+      "items": KnockoutFire.observableArray(firebaseRef)
+    };
     ko.applyBindings(viewModel, document.getElementById("items"));
+*/
+KnockoutFire = {};
+/*
+  KnockoutFire.observableArray(firebaseRef, options)
 
   Arguments:
     firebaseRef:
-      The firebase reference or query for collection.
+      The firebase reference or query.
     options:
   Options:
     reverseOrder: (true|false)
       Order the items in reverse order of their priority.
-    collectionName:
-      The name for data-bind="foreach: ..."
-    objExtendFunc: function(obj, firebaseRef): obj is each value of the collection.
-      Can be used to declare computed observable using ko.computed().
+    itemExtendFunc: function(item, itemRef)
+      Can be used to declare computed observable for each item
       Example:
-      new KnockoutFireCollectionViewModel(firebaseRef, "people", function(obj) {
-        obj.fullName = ko.computed(function() {
-          return obj.firstName() + " " + obj.lastName();
+      KnockoutFire.observableArray(firebaseRef, function(obj) {
+        item.fullName = ko.computed(function() {
+          return item.firstName() + " " + item.lastName();
         });
       });
 
@@ -33,74 +37,70 @@
       var firebaseRef = ko.contextFor(this).$data[".ref"];
       firebaseRef.remove();
     });
-
-  Todo:
-    - 
 */
-//function KnockoutFireCollectionViewModel(firebaseRef, collectionName, itemExtendFunc) {
-function KnockoutFireCollectionViewModel(firebaseRef, options) {
-    var self = this,
-    options = options || {};
-    var collectionName = options.collectionName || "items";
-    var collection = self[collectionName] = ko.observableArray([]);
-    var createItem = function(itemSnap) {
-        var ref = itemSnap.ref();
-        var item = {".ref": ref};
-        for (var name in itemSnap.val()) {
-            item[name] = ko.observable(itemSnap.val()[name]);
-            item[name][".name"] = name;
-            ref.child(name).on("value", function(valueSnap) {
-                item[valueSnap.name()](valueSnap.val());
-            });
-            item[name].subscribe(function(newValue) {
-                ref.child(this.target[".name"]).set(newValue);
-            });
-        }
-        // TODO: Using inheritance or like, not to extend each object.
-        if (options.objExtendFunc) {
-            options.objExtendFunc(item, ref);
-        }
-        return item;
-    }
+KnockoutFire.observableArray = function(firebaseRef, options) {
+    var array = ko.observableArray([]);
     firebaseRef.on("child_added", function(addedSnap) {
-        var item = createItem(addedSnap);
+        var item = KnockoutFire.observableProperties(addedSnap);
+        if (options.itemExtendFunc) {
+            options.itemExtendFunc(item, addedSnap.ref());
+        }
         if (options.reverseOrder) {
-            collection.unshift(item);
+            array.unshift(item);
         } else {
-            collection.push(item);
+            array.push(item);
         }
     });
     firebaseRef.on("child_removed", function(removedSnap) {
         var name = removedSnap.name();
-        collection.remove(function(item) {
+        array.remove(function(item) {
             return name == item()[".ref"].name();
         });
     });
     firebaseRef.on("child_moved", function(movedSnap, prevChildName) {
-        var i, len = collection().length, item = undefined;
+        var i, len = array().length, item = undefined;
         for (i=0; i < len; i++) {
-            if (collection()[i][".ref"].name() == movedSnap.name()) {
-                item = collection.splice(i, 1)[0];
+            if (array()[i][".ref"].name() == movedSnap.name()) {
+                item = array.splice(i, 1)[0];
                 break;
             }
         }
         if (prevChildName) {
             for (i=0; i < len - 1; i++) {
-                if (collection()[i][".ref"].name() == prevChildName) {
+                if (array()[i][".ref"].name() == prevChildName) {
                     break;
                 }
             }
             if (options.reverseOrder) {
-                collection.splice(i, 0, item);
+                array.splice(i, 0, item);
             } else {
-                collection.splice(i + 1, 0, item);
+                array.splice(i + 1, 0, item);
             }
         } else {
             if (options.reverseOrder) {
-                collection.unshift(item);
+                array.unshift(item);
             } else {
-                collection.push(item);
+                array.push(item);
             }
         }
     });
+    return array
+};
+/*
+  KnockoutFire.observableProperties(snapshot)
+*/
+KnockoutFire.observableProperties = function(snapshot) {
+    var ref = snapshot.ref();
+    var val = {".ref": ref};
+    for (var name in snapshot.val()) {
+        val[name] = ko.observable(snapshot.val()[name]);
+        val[name][".name"] = name;
+        ref.child(name).on("value", function(valueSnap) {
+            val[valueSnap.name()](valueSnap.val());
+        });
+        val[name].subscribe(function(newValue) {
+            ref.child(this.target[".name"]).set(newValue);
+        });
+    }
+    return val;
 };
